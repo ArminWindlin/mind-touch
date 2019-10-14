dfd02ce94790762eb7386f879beb3aff7d90d516
Armin Windlin <armin.windlin@swisscom.com>
2019-10-14 16:05:40 +0200

    implementet button onclick detection in menu

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,6 @@
 import javafx.application.Application;
-import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
-
-import java.util.Objects;
 
 public class Main extends Application {
 
@@ -16,49 +8,22 @@ public class Main extends Application {
     final int WINDOW_HEIGHT = 680;
 
     private Stage window;
-    private Scene scene1;
+    private Scene menuScene;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         window = primaryStage;
 
-        Group root = new Group();
-        Canvas canvas = new Canvas(WINDOW_WIDTH, WINDOW_HEIGHT);
-        root.getChildren().add(canvas);
-        scene1 = new Scene(root);
-
-        scene1.setOnMouseClicked(e -> window.setScene(new Game(this).getGameScene()));
-
-        scene1.setOnKeyPressed(e -> {
-            String code = e.getCode().toString();
-            if (Objects.equals(code, "SPACE") || Objects.equals(code, "ENTER")) {
-                window.setScene(new Game(this).getGameScene());
-            }
-        });
+        menuScene = Menu.getMenuScene(this, window);
 
         window.setTitle("MindTouch");
         window.setOnCloseRequest(e -> closeProgram());
-        window.setScene(scene1);
+        window.setScene(menuScene);
         window.show();
-        drawMenu(canvas);
     }
 
     void goToMenu() {
-        window.setScene(scene1);
-    }
-
-    private void drawMenu(Canvas canvas) {
-        GraphicsContext gc = canvas.getGraphicsContext2D();
-        gc.clearRect(0, 0, WINDOW_WIDTH, WINDOW_WIDTH);
-        gc.setFill(Color.WHITE);
-        gc.fillRect(0, 0, WINDOW_WIDTH, WINDOW_WIDTH);
-        gc.strokeRect(300, 150, 600, 100);
-        gc.setFont(Font.font("Verdana", FontWeight.BOLD, 60));
-        gc.setFill(Color.BLACK);
-        gc.fillText("PLAY", 500, 225);
-        gc.strokeRect(300, 350, 600, 100);
-        gc.setFill(Color.BLACK);
-        gc.fillText("LEVELS", 460, 425);
+        window.setScene(menuScene);
     }
 
     private void closeProgram() {
---
+++ src/Menu.java
@@ -0,0 +1,63 @@
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+class Menu {
+
+    static Scene getMenuScene(Main main, Stage window) throws Exception {
+
+        Group root = new Group();
+        Canvas canvas = new Canvas(main.WINDOW_WIDTH, main.WINDOW_HEIGHT);
+        root.getChildren().add(canvas);
+        Scene scene = new Scene(root);
+
+        scene.setOnMouseClicked(e -> {
+            int x = (int) Math.floor(e.getX());
+            int y = (int) Math.floor(e.getY());
+
+            // play button
+            if (isInsideRect(300, 150, 600, 100, x, y))
+                window.setScene(new Game(main).getGameScene());
+
+            // levels button
+            if (isInsideRect(300, 350, 600, 100, x, y))
+                System.out.println("Levels");
+        });
+
+        scene.setOnKeyPressed(e -> {
+            String code = e.getCode().toString();
+            if (Objects.equals(code, "SPACE") || Objects.equals(code, "ENTER")) {
+                window.setScene(new Game(main).getGameScene());
+            }
+        });
+
+        drawMenu(canvas, main.WINDOW_WIDTH, main.WINDOW_HEIGHT);
+
+        return scene;
+    }
+
+    private static void drawMenu(Canvas canvas, int width, int height) {
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        gc.clearRect(0, 0, height, width);
+        gc.setFill(Color.WHITE);
+        gc.fillRect(0, 0, height, width);
+        gc.strokeRect(300, 150, 600, 100);
+        gc.setFont(Font.font("Verdana", FontWeight.BOLD, 60));
+        gc.setFill(Color.BLACK);
+        gc.fillText("PLAY", 500, 225);
+        gc.strokeRect(300, 350, 600, 100);
+        gc.setFill(Color.BLACK);
+        gc.fillText("LEVELS", 460, 425);
+    }
+
+    private static boolean isInsideRect(int rectX, int rectY, int rectWidth, int rectHeight, int x, int y) {
+        return x > rectX && x < rectX + rectWidth && y < rectY + rectHeight && y > rectY;
+    }
+}
