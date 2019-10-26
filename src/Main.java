86b683683ab42efeb264f8af46d5618ccae45a38
ArminWindlin <armin.windlin@gmail.com>
2019-10-26 12:49:04 +0200

    implemented level 10, disabled resizing of window

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         window = primaryStage;
+        primaryStage.setResizable(false);
 
         menuScene = Menu.getMenuScene(this);
 
